b79b329b09da1b9e2b96e18cdc38bd8f5b50d0d5
pooja-kh-26 <poojakh2026@gmail.com>
2024-12-28 12:04:30 +0530

    first-commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <div className = "max-w-screen-2xl container ma-auto px-4 md:px-20">
+            <div>
+                
+            </div>
+            <div></div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar;
